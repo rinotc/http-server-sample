3cac3d745da61d486656d37f26a9a2e25bb9be08
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-07-17 15:03:56 +0900

    AbstractHttpMessage実装

---
+++ src/main/scala/com/github/rinotc/http/server/AbstractHttpMessage.scala
@@ -0,0 +1,27 @@
+package com.github.rinotc.http.server
+
+import java.nio.charset.StandardCharsets
+import scala.collection.mutable
+
+abstract class AbstractHttpMessage(
+    val headers: mutable.Map[String, String] = new mutable.HashMap[String, String](),
+    protected var body: Array[Byte] = Array.emptyByteArray
+) {
+
+  def addHeaderField(name: String, value: String): Unit = {
+    headers.put(name, value)
+  }
+
+  def setBody(body: Array[Byte]): Unit = {
+    this.body = body
+  }
+
+  def getBody: Array[Byte] = this.body
+
+  protected def startLine: String
+
+  override def toString: String = {
+    val bodyString = new String(body, StandardCharsets.UTF_8)
+    s"$startLine headers: $headers body: $bodyString"
+  }
+}
